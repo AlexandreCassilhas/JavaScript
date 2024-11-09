389bcafd95f1b810cce5b033730a6a0b39562248
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2024-11-09 19:19:04 -0300

    New exercise

---
+++ livroJS/cap06/js/resp6_a.js
@@ -0,0 +1,5 @@
+// Inicializando o form e as variáveis
+const frm = document.querySelector('form')
+const entClube = document.querySelector('#inClube')
+const saiResp = document.querySelector('#outResp')
+const clubes = []
