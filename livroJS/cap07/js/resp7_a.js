3299fe41ad81e2d60527b0c5b0f7fc5366e384ca
Alexandre Cassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2024-11-17 21:17:07 -0300

    New  exercise

---
+++ livroJS/cap07/js/resp7_a.js
@@ -0,0 +1,24 @@
+const frm = document.querySelector('form')
+const inMensagem = document.querySelector('#inMensagem')
+const outCripto = document.querySelector('#outCripto')
+
+frm.addEventListener('submit', (criptografarMsg) =>{
+  criptografarMsg.preventDefault()
+  const mensagem = inMensagem.value
+  const tamanho = mensagem.length
+  let listaPares = ''
+  let listaImpares = ''
+  for(let i = 0; i < tamanho; i++){
+    if (i % 2 != 0){
+      listaPares += mensagem.substr(i, 1)
+    } else if (i % 2 == 0){
+      listaImpares += mensagem.substr(i, 1)
+    }
+  }
+  outCripto.innerText = `${listaPares}${listaImpares}`
+})
+
+frm.btnDecripto.addEventListener('click', (decriptoMsg) =>{
+  const Msg = inMensagem.value
+  outCripto.innerText = `${Msg}`
+})
