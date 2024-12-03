73e97d0b217db03a548db3b8f132f7e2b4cf7842
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2024-12-03 19:52:41 -0300

    Update resp8_c.js

--- livroJS/cap08/js/resp8_c.js
+++ livroJS/cap08/js/resp8_c.js
@@ -1,3 +1,4 @@
+// Instanciando as variáveis do DOM
 const frm = document.querySelector('form')
 const inValor = document.querySelector('#inValor')
 const labelConvenio = document.querySelector('.labelConvenio')
@@ -5,34 +6,35 @@ const inConvenio = document.querySelector('#inConvenio')
 const rbSim = document.querySelector('#rbSim')
 const rbNao = document.querySelector('#rbNao')
 const outResp = document.querySelector('#outResp')
-
+// Exibindo a caixa de seleção e o label
 frm.rbSim.addEventListener('click', () =>{
   labelConvenio.className = 'exibe'
   inConvenio.className = 'exibe'
 })
-
+// Ocultando a caixa de seleção e o label
 frm.rbNao.addEventListener('click', () =>{
   labelConvenio.className = 'oculta'
   inConvenio.className = 'oculta'
 })
-
+// Criando o listener do botão
 frm.addEventListener('submit', (e) =>{
   e.preventDefault()
+  let convenio = ''
   const valor = Number(inValor.value)
   if(rbSim.checked){
-    const num = inConvenio.selectIndex
-    const convenio = inConvenio.options[num].text
+    const num = inConvenio.selectedIndex
+    convenio = inConvenio.options[num].text
   } else {
-    const convenio = 'Sem Convênio'
+    convenio = 'Sem Convênio'
   }
   const desconto = calcularDesconto(valor, convenio)
   const pagar = valor - desconto
-
-  outResp.innerText = `Desconto: R$ ${desconto}\nA pagar: ${pagar}`
+  // Apresentando o resultado no DOM
+  outResp.innerText = `Valor Cheio: R$ ${valor}\nDesconto: R$ ${desconto.toFixed(2)}\nA pagar: R$ ${pagar.toFixed(2)}`
 })
-
+// Função para calcular o desconto pelo convênio
 function calcularDesconto(valor, convenio) {
-  const percentual = convenio == 'Sem convênio' ? 10 : convenio == 'Amigo dos Animais' ? 20 : 50
+  const percentual = convenio == 'Sem Convênio' ? 10 : convenio == 'Amigo dos Animais' ? 20 : 50
   const desconto = valor * percentual / 100
   return desconto
 }
