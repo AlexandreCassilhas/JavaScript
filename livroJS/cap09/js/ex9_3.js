dd93f1af2996c192a7009b9da58e42602d93aeee
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2024-12-22 16:00:03 -0300

    Update ex9_3.js

--- livroJS/cap09/js/ex9_3.js
+++ livroJS/cap09/js/ex9_3.js
@@ -55,3 +55,52 @@ const mostrarApostas = () => {
 }
 // Chama a função quando a página é carregada.
 window.addEventListener('load', mostrarApostas())
+
+frm.btVencedor.addEventListener('click', (e) =>{
+  // Verifica se há apostas armazenadas
+  if(!localStorage.getItem('jogoMelanciaNome')){
+    alert(`Não há apostas cadastradas!`)
+    return
+  }
+  // solicita que seja digitado o peso correto da melancia
+  const pesoCorreto = Number(prompt('Digite o peso correto da melancia: '))
+  if(pesoCorreto == 0 || isNaN(pesoCorreto)){
+    alert('Digite um número superior a zero para o peso da melancia')
+    return
+  }
+  // recupera os dados dos apostadores
+  const nomes = localStorage.getItem('jogoMelanciaNome').split(';')
+  const pesos = localStorage.getItem('jogoMelanciaPeso').split(';')
+
+  let vencedorNome = nomes[0]
+  let vencedorPeso = pesos[0]
+
+  // Percorre as apostas em busca do vencedor
+  for(let i = 1; i < nomes.length; i++){
+    const difVencedor = Math.abs(vencedorPeso - pesoCorreto)
+    const difAposta = Math.abs(Number(pesos[i]) - pesoCorreto)
+    if(difAposta < difVencedor){
+      vencedorNome = nomes[i]
+      vencedorPeso = pesos[i]
+    }
+  }
+  // Calcula a diferença entre o peso mais próximo e o peso Correto
+  const diferenca = vencedorPeso == pesoCorreto ? 'Acertou na Mosca!': `A diferença foi de ${Math.abs(vencedorPeso - pesoCorreto)} gr`
+
+  // Monta a mensagem de saída, mostrando o resultado da aposta
+  let mensagem = `Resultado da Aposta do Peso da Melancia: \n`
+  mensagem += `Peso correto da melancia: ${pesoCorreto} gr\n`
+  mensagem += `Vencedor: ${vencedorNome}\n`
+  mensagem += `Peso apostado: ${vencedorPeso} gr\n`
+  mensagem += diferenca
+
+  alert(mensagem)
+})
+// Evento click para limpar os dois localStorages
+frm.btLimpar.addEventListener('click', (e) =>{
+  if(confirm('Confirma a exclusão de todas as apostas')){
+    localStorage.removeItem('jogoMelanciaNome')
+    localStorage.removeItem('jogoMelanciaPeso')
+    mostrarApostas()
+  }
+})
