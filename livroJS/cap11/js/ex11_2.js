d04f7d6a36b471fefdd1f9209c8df219b3904d85
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2025-04-03 18:28:10 -0300

    Update ex11_2.js

--- livroJS/cap11/js/ex11_2.js
+++ livroJS/cap11/js/ex11_2.js
@@ -2,17 +2,21 @@ const frm = document.querySelector('form')
 const divPalco = document.getElementById('divPalco')
 
 const POLTRONAS = 240 // const com o nº de poltronas do teatro
-const reservadas = []
+let reservadas = []
+let ocupadas = []
 
 window.addEventListener('load', () =>{
   //operador ternário. Se houver dados em localStorage, faz um split e atribui a um array
-  const reservadas = localStorage.getItem('storageReservadas') 
+  const StReservadas = localStorage.getItem('storageReservadas') 
     ? localStorage.getItem('storageReservadas').split(',')
     : []
 
-  const ocupadas = localStorage.getItem('storageOcupadas') 
+  const StOcupadas = localStorage.getItem('storageOcupadas') 
     ? localStorage.getItem('storageOcupadas').split(',') 
     : []
+  
+  reservadas = StReservadas.slice()
+  ocupadas = StOcupadas.slice()
   
     // loop para montar as poltronas
     for(let i = 1; i<= POLTRONAS; i++){
@@ -55,96 +59,87 @@ window.addEventListener('load', () =>{
 
 frm.addEventListener('submit', (e) =>{
   e.preventDefault()
-
+  // recupera o nº digitado no input do DOM
   const poltrona = Number(frm.inPoltrona.value)
 
-  verificaStatusPoltrona(poltrona) // função para checar poltrona
-
-  // captura a img da poltrona. (-1 pois inicia em 0)
-  const imgPoltrona = divPalco.querySelectorAll('img')[poltrona - 1]
-
-  if(verificaStatusPoltrona()){
-    imgPoltrona.src = 'img/reservada.png' // modifica a imagem
-  }
-
-  reservadas.push(poltrona) // insere o nº da poltrona no array 'reservadas'
-
-  // inclui na localStorage
-  if(localStorage.getItem('storageReservadas')){
-    localStorage.setItem('storageReservadas', localStorage.getItem('storageReservadas') + ',' + poltrona)
-  } else {
-    localStorage.setItem('storageReservadas', poltrona)
-  }
-  frm.inPoltrona.value = ''
-  frm.inPoltrona.focus()
+  reservaPoltrona(poltrona) // função para checar poltrona
 })
 
-function verificaStatusPoltrona(poltrona){
+function reservaPoltrona(poltrona){
+
+  // recupera as poltronas ocupadas e insere no array
+  //const ocupadas = localStorage.getItem('storageOcupadas') 
+   // ? localStorage.getItem('storageOcupadas').split(',')
+  //  : []
+  // realiza as checagens necessárias para a validação
   if(poltrona > POLTRONAS || poltrona < 1){
     alert(`A poltrona ${poltrona} é inválida! Digite um número entre 1 e ${POLTRONAS}.`)
     frm.inPoltrona.value = ''
     frm.inPoltrona.focus()
     return
-  } 
-
-  const reservadas = localStorage.getItem('storageReservadas') 
-    ? localStorage.getItem('storageReservadas').split(',') 
-    : []
-
-  const ocupadas = localStorage.getItem('storageOcupadas') 
-    ? localStorage.getItem('storageOcupadas').split(',')
-    : []
-  
-  if(reservadas.includes.toString()){
+  } else if(reservadas.includes(poltrona)){
     alert(`A poltrona ${poltrona} já está reservada! Escolha outro assento.`)
     frm.inPoltrona.value = ''
     frm.inPoltrona.focus()
     return 
-  }
-
-  if(ocupadas.includes(poltrona.toString())){
+  } else if(ocupadas.includes(poltrona)){
     alert(`A poltrona ${poltrona} já se encontra ocupada! Escolha outro assento.`)
     frm.inPoltrona.value = ''
     frm.inPoltrona.focus()
-    return false
-  }
+    return
+  } else {  
+    // captura a img da poltrona. (-1 pois inicia em 0)
+    const imgPoltrona = divPalco.querySelectorAll('img')[poltrona - 1]
   
+    imgPoltrona.src = 'img/reservada.png' // altera a imagem para reservada
+  
+    reservadas.push(poltrona) // insere o nº da poltrona no array 'reservadas'
+  
+    // inclui o nº da poltrona na localStorage 
+    if(localStorage.getItem('storageReservadas')){
+      localStorage.setItem('storageReservadas', localStorage.getItem('storageReservadas') + ',' + poltrona)
+    } else {
+      localStorage.setItem('storageReservadas', poltrona)
+    }
+    frm.inPoltrona.value = ''
+    frm.inPoltrona.focus()
+  }
 }
 
 frm.btnConfirmar.addEventListener('click', () =>{
 
-  const reservadas = localStorage.getItem('storageReservadas').split(',')
+  const StReservadas = localStorage.getItem('storageReservadas').split(',')
 
   // checa se há poltronas reservadas
-  if(reservadas.length == 0){
+  if(StReservadas.length == 0){
     alert('Não há poltronas reservadas!')
     frm.inPoltrona.focus()
     return
   }
 
-  const ocupadas = localStorage.getItem('storageOcupadas') 
+  const StOcupadas = localStorage.getItem('storageOcupadas') 
     ? localStorage.getItem('storageOcupadas').split(',') 
     : []
 
   // loop decrescente pois as reservas vão sendo removidas a cada alteração da imagem
-  for(let i = reservadas.length - 1; i >= 0; i--){
-    ocupadas.push(reservadas[i])
+  for(let i = StReservadas.length - 1; i >= 0; i--){
+    StOcupadas.push(reservadas[i])
 
-    const imgPoltrona = divPalco.querySelectorAll('img')[reservadas[i] - 1] // captura img da poltrona
+    const imgPoltrona = divPalco.querySelectorAll('img')[StReservadas[i] - 1] // captura img da poltrona
     imgPoltrona.src = 'img/ocupada.png' // altera a imagem para ocupada
 
-    reservadas.pop() // remove o número da poltrona do array
+    StReservadas.pop() // remove o número da poltrona do array
   }
   
   // recriando a localStorage 'Reservadas'
   localStorage.removeItem('storageReservadas')
-  for(let i = 0; i < reservadas.length; i++){
-    if(i < reservadas.length - 1){
-      localStorage.setItem('storageReservadas', reservadas[i] + ',')
+  for(let i = 0; i < StReservadas.length; i++){
+    if(i < StReservadas.length - 1){
+      localStorage.setItem('storageReservadas', StReservadas[i] + ',')
     } else {
-      localStorage.setItem('storageReservadas', reservadas[i])
+      localStorage.setItem('storageReservadas', StReservadas[i])
     }
 
   }
-  localStorage.setItem('storageOcupadas', ocupadas.join(','))
+  localStorage.setItem('storageOcupadas', StOcupadas.join(','))
 })
