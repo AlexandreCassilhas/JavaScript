08e63975078ded0f6882f8799f89a28292e994be
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2025-05-03 12:58:35 -0300

    Update app.js

--- livroJS/cap12/app.js
+++ livroJS/cap12/app.js
@@ -10,6 +10,16 @@ app.get('/cap12', (req, res) => {
   res.send('<h2>Capítulo 12: Introdução ao Express. Após a falha e clone do repositório. Sucesso!!!</h2>')
 })
 
+// middleware informando que será utilizado o formato JSON para troca de informações
+app.use(express.json())
+
+app.post('/filmes', (req, res) => {
+  const titulo = req.body.titulo
+  const genero = req.body.genero
+  // poderia ser const {titulo, genero} = req.body
+  res.send(`Filme; ${titulo} - Gênero: ${genero}, recebido...`)
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${3001}`)
 })
