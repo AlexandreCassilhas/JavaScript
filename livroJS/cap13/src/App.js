782c33578c4949db7303ab8e1d00e4708337ac08
AlexandreCassilhas <50409766+AlexandreCassilhas@users.noreply.github.com>
2025-05-23 17:08:25 -0300

    React Installation

---
+++ livroJS/cap13/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+import MenuSuperior from './components/MenuSuperior';
+import InclusaoLivros from './components/InclusaoLivros';
+
+function App() {
+  return (
+    <>
+       <MenuSuperior />
+       <InclusaoLivros />
+    </>
+  );
+}
+
+export default App;
